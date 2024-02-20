af281222c362e1bf5fe88fd60e3fc1f64c871f50
Contra07 <nano2012rus@yandex.ru>
2024-02-20 23:07:39 +0400

    JdbcTemplate DAO без RowFormatter

--- todo/src/main/java/ru/ssau/todo/service/ProjectDAO.java
+++ todo/src/main/java/ru/ssau/todo/service/ProjectDAO.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.lang.NonNull;
 
 import ru.ssau.todo.api.model.Project;
 
@@ -14,43 +13,74 @@ public class ProjectDAO implements IDataAccessObject<Project> {
     
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
-    @Autowired
-    @NonNull
-    private ProjectRowMapper rowMapper;
 
     @Override
-    public void Create(Project t) {
-        // TODO Auto-generated method stub
+    public void Create(Project project) {
+        String sql = "INSERT INTO public.\"Project\" (\"Description\" = ?, \"StartDate\" = ?, \"EndDate\" = ?) VALUES ( ?, ?, ?)";
+        int result = 0;
+        try {
+            result = jdbcTemplate.update(sql, project.getDescription(), project.getStartDate(), project.getEndDate());
+        }
+        catch (DataAccessException ex) {
+            
+        }
+        if(result == 0) {
+            
+        }
     }
 
     @Override
     public void Delete(int id) {
-
+        String sql = "DELETE FROM public.\"Project\" WHERE \"Id\" = ?";
+        int result = 0;
+        try {
+            result = jdbcTemplate.update(sql);
+        }
+        catch (DataAccessException ex) {
+            
+        }
+        if(result == 0) {
+            
+        }
     }
 
     @Override
     public Optional<Project> Get(int id) {
-        String sql = "SELECT * FROM Project WHERE Id = ?";
-        Project course = null;
+        String sql = "SELECT * FROM public.\"Project\" WHERE \"Id\" = ?";
+        Project result = null;
         try {
-            course = jdbcTemplate.queryForObject(sql, rowMapper, id);
+            result = jdbcTemplate.queryForObject(sql, Project.class, id);
         }
         catch (DataAccessException ex) {
-            course = null;
+            result = null;
         }
-        return Optional.ofNullable(course);
+        return Optional.ofNullable(result);
     }
 
     @Override
     public List<Project> List() {
-        // TODO Auto-generated method stub
-        return null;
+        String sql = "SELECT * FROM public.\"Project\"";
+        try {
+            return jdbcTemplate.queryForList(sql, Project.class);
+        }
+        catch (DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
-    public void Update(Project t, int id) {
-        // TODO Auto-generated method stub
+    public void Update(Project project, int id) {
+        String sql = "UPDATE public.\"Project\" SET \"Description\" = ?, \"StartDate\" = ?, \"EndDate\" = ? WHERE \"Id\" = ?";
+        int result = 0;
+        try {
+            result = jdbcTemplate.update(sql, project.getDescription(), project.getStartDate(), project.getEndDate(), id);
+        }
+        catch (DataAccessException ex) {
+            
+        }
+        if(result == 0) {
+            
+        }
         
     }
 }
