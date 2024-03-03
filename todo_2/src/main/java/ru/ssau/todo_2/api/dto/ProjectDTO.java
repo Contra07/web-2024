7290392f98e6d75c8eda1afe3fdf07da8b85435c
Contra07 <nano2012rus@yandex.ru>
2024-03-03 19:36:00 +0400

    Без списка задач

--- todo_2/src/main/java/ru/ssau/todo_2/api/dto/ProjectDTO.java
+++ todo_2/src/main/java/ru/ssau/todo_2/api/dto/ProjectDTO.java
@@ -1,15 +1,12 @@
 package ru.ssau.todo_2.api.dto;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.ssau.todo_2.data.entity.Project;
-import ru.ssau.todo_2.data.entity.Task;
 
 @Data
 @NoArgsConstructor
@@ -20,19 +17,13 @@ public class ProjectDTO {
     private String description;
     private LocalDate startDate;
     private LocalDate endDate;
-    private List<TaskDTO> tasks;
 
     public static ProjectDTO fromEntity(Project project) {
-        List<TaskDTO> tasksDTO = new ArrayList<>();
-        for (Task task : project.getTasks()) {
-            tasksDTO.add(TaskDTO.fromEntity(task));
-        }
         return new ProjectDTO(
                 project.getId(),
                 project.getName(),
                 project.getDescription(),
                 project.getStartDate(),
-                project.getStartDate(),
-                tasksDTO);
+                project.getStartDate());
     }
 }
