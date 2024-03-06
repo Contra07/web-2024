fc4df90a3c35f9e592dd9ad11bcf3f78873a6a84
Contra07 <nano2012rus@yandex.ru>
2024-03-06 16:31:12 +0400

    Pojo to Entity

--- todo_2/src/main/java/ru/ssau/todo_2/api/dto/ProjectPojo.java
+++ todo_2/src/main/java/ru/ssau/todo_2/api/dto/ProjectPojo.java
@@ -1,29 +1,42 @@
 package ru.ssau.todo_2.api.dto;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.ssau.todo_2.data.entity.Project;
+import ru.ssau.todo_2.data.entity.Task;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProjectDTO {
+public class ProjectPojo {
     private UUID id;
     private String name;
     private String description;
     private LocalDate startDate;
     private LocalDate endDate;
 
-    public static ProjectDTO fromEntity(Project project) {
-        return new ProjectDTO(
+    public static ProjectPojo fromEntity(Project project) {
+        return new ProjectPojo(
                 project.getId(),
                 project.getName(),
                 project.getDescription(),
                 project.getStartDate(),
                 project.getStartDate());
+    }
+
+    public static Project toEntity(ProjectPojo projectDTO){
+        return new Project(
+            projectDTO.id,
+            projectDTO.name,
+            projectDTO.description,
+            projectDTO.startDate,
+            projectDTO.endDate,
+            new HashSet<Task>()
+        );
     }
 }
--- todo_2/src/main/java/ru/ssau/todo_2/api/dto/TaskPojo.java
+++ todo_2/src/main/java/ru/ssau/todo_2/api/dto/TaskPojo.java
@@ -11,19 +11,30 @@ import ru.ssau.todo_2.data.entity.Task;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TaskDTO {
+public class TaskPojo {
     private UUID id;
     private String name;
     private String description;
     private LocalDate completionDate;
     private boolean done;
 
-    public static TaskDTO fromEntity(Task task) {
-        return new TaskDTO(
+    public static TaskPojo fromEntity(Task task) {
+        return new TaskPojo(
                 task.getId(),
                 task.getName(),
                 task.getDescription(),
                 task.getCompletionDate(),
                 task.isDone());
+    }
+
+    public static Task toEntity(TaskPojo taskDTO){
+        return new Task(
+            taskDTO.getId(),
+            null,
+            taskDTO.getName(),
+            taskDTO.getDescription(),
+            taskDTO.getCompletionDate(),
+            taskDTO.isDone()
+        );
     }
 }
