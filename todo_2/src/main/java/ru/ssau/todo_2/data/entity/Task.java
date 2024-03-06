2927d5b144756b5ee84c0f2dd5d9bdfa001a0f5c
Contra07 <nano2012rus@yandex.ru>
2024-03-06 16:12:22 +0400

    Связь с проектом

--- todo_2/src/main/java/ru/ssau/todo_2/data/entity/Task.java
+++ todo_2/src/main/java/ru/ssau/todo_2/data/entity/Task.java
@@ -3,10 +3,11 @@ package ru.ssau.todo_2.data.entity;
 import java.time.LocalDate;
 import java.util.UUID;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,8 +30,8 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private UUID id;
-    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "projectId")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "projectId", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Project project;
     @Column(name = "name", length = 100)
     private String name;
